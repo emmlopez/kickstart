cf649ab82cab75ed400b159a63f4699680d8e947
Emmanuel <emmanuel@Emmanuels-MacBook-Pro.local>
2014-04-24 18:38:57 -0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kickstartApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
